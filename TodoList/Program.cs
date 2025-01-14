9acab133e14bc3c74baa477df2f00509155e2905
ErrQD0303 <quocdat3396@gmail.com>
2025-01-14 20:36:26 +0700

    Fix Connection String for TodoList Project

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 // builder.Services.AddSingleton<ITodoItemRepository, InMemoryTodoItemRepository>();
-builder.Services.AddDbContext<SQLServerWebContext>();
+builder.Services.AddDbContext<SQLServerWebContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ITodoItemRepository, SQLServerTodoItemRepository<SQLServerWebContext>>();
 
 builder.Services.AddTransient<TodoListManager>();
